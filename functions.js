da16b5a602c3a98fbd56175c5175286046ff2e17
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-08 09:52:30 -0800

    reformatted add/undo/close poll buttons in html

---
+++ functions.js
@@ -0,0 +1,20 @@
+displayCurrentPoll() {
+    // clear out the current poll div
+    currentContainerEl.textContent = '';
+
+}
+
+
+
+displayAllPolls()
+
+
+
+makePoll()
+
+
+
+renderPoll(poll)
+
+
+renderOption(optionA, optionB)
